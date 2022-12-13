d8830b81abc465943ef6462160cf29760dee2e02
suntaehun <sth7862@naver.com>
2022-12-13 15:57:16 +0900

    java script 5

--- javascript-basics/basics/ex04.js
+++ javascript-basics/basics/ex04.js
@@ -11,6 +11,35 @@
     - 자바와 같은 블록 스코프를 지원한다.
     - let 키워드를 붙여서 블록 범위를 가지게 한다.
     - const 키워드는 블록 범위에 있는 상수에 적용한다.
-*/
+5. 결론
+    const / let 만 사용하고 둘 중에 하나를 반드시 붙히자!!!
+    */
+
+var i = 10;
+var f = function() {
+    var i = 20;
+    j = 100;
+    console.log(i);
+    i = j - i;
+}
+f();
+console.log(i);
+console.log(j);
+
+console.log("==========var 키워드는 함수 블록에서만 함수 범위를 만든다.==========");
+if(90 + 10 === 100) {
+    var k = 20;
+}
+console.log(k);
+
+{
+    let x = 1000;
+    const PI = 3.15;
+
+    x = 100;
+    // PI = 0; // assign error
+}
+// console.log(x);  // not defined error
+// console.log(PI); // not defined error
 
 
