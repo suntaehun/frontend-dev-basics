dd66d5037ef8b083a18ee70a27af4a0397c23312
suntaehun <sth7862@naver.com>
2022-12-13 10:14:16 +0900

    data type

---
+++ javascript-basics/basics/ex02.js
@@ -0,0 +1,22 @@
+/*
+변수와 자료형(Data Type)
+
+[기본타입(Primitive Type)]
+- undefined
+- number
+- string
+- boolean
+[객체]
+
+*/
+var u = undefined; // var u; 동일, 선언 정의가 구분이 없다.
+var i = 10;
+var s = "Hello World";
+var b = true;
+
+console.log(u + " : " + typeof(u));
+console.log(i + " : " + typeof(i));
+console.log(s + " : " + typeof(s));
+console.log(b + " : " + typeof(b));
+console.log(s.length);
+console.log(s.toUpperCase());
