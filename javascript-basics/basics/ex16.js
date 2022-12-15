7fc5a5dd6dfb809feabcd8acf8725c8a4cb22eeb
suntaehun <sth7862@naver.com>
2022-12-15 09:59:25 +0900

    function

---
+++ javascript-basics/basics/ex16.js
@@ -0,0 +1,68 @@
+/*
+함수 : function 타입의 객체
+*/
+
+// 함수를 생성하는 방법 1: 함수 리터럴
+console.log("==========함수를 생성하는 방법 1==========");
+function f1(a, b) {
+    return a + b;
+}
+console.log(typeof(f1), f1 instanceof Function, f1(10, 20));
+// 함수를 생성하는 방법 2: 함수 리터럴(추천)
+console.log("==========함수를 생성하는 방법 2==========");
+var f2 = function(a, b) {
+    return a + b;
+}
+console.log(typeof(f2), f2 instanceof Function, f2(10, 20));
+
+// 함수를 생성하는 방법 3: new 연산자와 함께 Function 내장 객체(생성자 함수)를 사용하는 방법
+console.log("==========함수를 생성하는 방법 3==========");
+var f3 = new Function("a", "b", "return a + b;");
+console.log(typeof(f3), f3 instanceof Function, f3(10, 20));
+
+// 함수를 생성하는 방법 4: 익명(anonymous) 함수: callback
+setTimeout(function() {
+    console.log('time out!');
+}, 1000);
+
+// 익명(anonymous) 함수의 다른 예: 한번만(즉시) 실행하는 함수
+console.log("==========익명(anonymous) 함수의 다른 예==========");
+var s = (function(a, b) {
+    var m = 10;
+    return a/m + b/m;
+})(10, 20);
+
+console.log(s);
+
+// 가변 파라미터
+// 1. 자바스크립트의 함수는 기본적으로 가변 파라미터를 지원
+//    파라미터는 고정되어 있지 않다.
+//    에러가 나지 않음.
+var f4 = function(a, b, c) {
+    console.log(a, b, c);
+}
+f4(10, 20, 30, 40, 50, 60);
+
+// 2. 예
+console.log("==========가변 파라미터 함수 sum()==========");
+var sum = function() {
+    var s = 0;
+
+    console.log(arguments instanceof Array, arguments.length);
+    for(let i = 0; i < arguments.length; i++) {
+        s += arguments[i];
+    }
+
+    // Error
+    // arguments의 __proto__는 Object prototype에 chain이 되어있기 때문에 오류!
+    // arguments.forEach(function(e) {
+        s += e;
+    // });
+    return s;
+}
+
+console.log(sum(1));
+console.log(sum(1, 2));
+console.log(sum(1, 2, 3));
+console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4, 5, 6));
